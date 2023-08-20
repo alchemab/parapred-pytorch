d6345ae79fef1fda739b288e0191f63572bd1ea6
ideasbyjin <>
2023-08-20 22:34:53 +0100

    Roll up to PyTorch 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="parapred-pytorch",
-    version="1.0.1",
+    version="1.0.2",
     author = "Alchemab",
     author_email = "jin@alchemab.com",
     description="PyTorch implementation of Parapred",
